d07799665970485a73a539704fe7536a1b8b8786
taraasik <taraasik@gmail.com>
2020-04-29 22:13:04 -0400

    commenting out portfolio images

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -14,13 +14,13 @@ const Portfolio = props => (
       <div className="row">
         <div className="col-4 col-12-mobile">
           <article className="item">
-            <a href="#" className="image fit"><img src="images/pic02.jpg" alt=""/></a>
+            {/*<a href="#" className="image fit"><img src="images/pic02.jpg" alt=""/></a>*/}
             <header>
               <h3>Project 1</h3>
             </header>
           </article>
           <article className="item">
-            <a href="#" className="image fit"><img src="images/pic03.jpg" alt=""/></a>
+            {/*<a href="#" className="image fit"><img src="images/pic03.jpg" alt=""/></a>*/}
             <header>
               <h3>Project 4</h3>
             </header>
@@ -28,13 +28,13 @@ const Portfolio = props => (
         </div>
         <div className="col-4 col-12-mobile">
           <article className="item">
-            <a href="#" className="image fit"><img src="images/pic04.jpg" alt=""/></a>
+            {/*<a href="#" className="image fit"><img src="images/pic04.jpg" alt=""/></a>*/}
             <header>
               <h3>Project 2</h3>
             </header>
           </article>
           <article className="item">
-            <a href="#" className="image fit"><img src="images/pic05.jpg" alt=""/></a>
+            {/*<a href="#" className="image fit"><img src="images/pic05.jpg" alt=""/></a>*/}
             <header>
               <h3>Project 5</h3>
             </header>
@@ -42,13 +42,13 @@ const Portfolio = props => (
         </div>
         <div className="col-4 col-12-mobile">
           <article className="item">
-            <a href="#" className="image fit"><img src="images/pic06.jpg" alt=""/></a>
+            {/*<a href="#" className="image fit"><img src="images/pic06.jpg" alt=""/></a>*/}
             <header>
               <h3>Project 3</h3>
             </header>
           </article>
           <article className="item">
-            <a href="#" className="image fit"><img src="images/pic07.jpg" alt=""/></a>
+            {/*<a href="#" className="image fit"><img src="images/pic07.jpg" alt=""/></a>*/}
             <header>
               <h3>Project 6</h3>
             </header>
