fb79e6511877e1b57f94487017185cff833375c4
taraasik <taraasik@gmail.com>
2020-04-29 21:48:06 -0400

    moving scripts to html.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,5 @@
 import React from "react"
 import { Helmet } from "react-helmet"
-import { withPrefix } from "gatsby"
 import Header from "./header/header"
 import Footer from "./footer"
 
@@ -8,16 +7,6 @@ export default ({ pageTitle, children }) => (
   <>
     <Helmet>
       <title>{`Elena Tarasova | ${pageTitle}`}</title>
-      <script src={'https://code.jquery.com/jquery-3.4.1.min.js'}
-              integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
-              crossOrigin="anonymous"
-              type="text/javascript" />
-      <script src={withPrefix('js/jquery.scrolly.min.js')} type="text/javascript" />
-      <script src={withPrefix('js/jquery.scrollex.min.js')} type="text/javascript" />
-      <script src={withPrefix('js/browser.min.js')} type="text/javascript" />
-      <script src={withPrefix('js/breakpoints.min.js')} type="text/javascript" />
-      <script src={withPrefix('js/util.js')} type="text/javascript" />
-      <script src={withPrefix('js/main.js')} type="text/javascript" />
     </Helmet>
     <div className="is-preload">
       <Header/>
---
+++ src/html.js
@@ -0,0 +1,46 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+export default function HTML(props) {
+  return (
+    <html {...props.htmlAttributes}>
+      <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <script src={'https://code.jquery.com/jquery-3.4.1.min.js'}
+                integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+                crossOrigin="anonymous"
+                type="text/javascript" />
+        {props.headComponents}
+      </head>
+      <body {...props.bodyAttributes}>
+        {props.preBodyComponents}
+        <div
+          key={`body`}
+          id="___gatsby"
+          dangerouslySetInnerHTML={{ __html: props.body }}
+        />
+        {props.postBodyComponents}
+        <script src={'js/jquery.scrolly.min.js'} type="text/javascript" />
+        <script src={'js/jquery.scrollex.min.js'} type="text/javascript" />
+        <script src={'js/browser.min.js'} type="text/javascript" />
+        <script src={'js/breakpoints.min.js'} type="text/javascript" />
+        <script src={'js/util.js'} type="text/javascript" />
+        <script src={'js/main.js'} type="text/javascript" />
+      </body>
+    </html>
+  )
+}
+
+HTML.propTypes = {
+  htmlAttributes: PropTypes.object,
+  headComponents: PropTypes.array,
+  bodyAttributes: PropTypes.object,
+  preBodyComponents: PropTypes.array,
+  body: PropTypes.string,
+  postBodyComponents: PropTypes.array,
+}
